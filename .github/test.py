a9ca84f4bc1517cb1bf1560b80b65b9fb3307b9e
yogeshpsp <67696309+yogeshpsp@users.noreply.github.com>
2023-09-21 14:19:46 +0530

    Update test.py

--- .github/test.py
+++ .github/test.py
@@ -1,6 +1,6 @@
 import yaml
 with open ('test.yaml','r') as file:
   yaml_content=yaml.safe_load(file)
-data=yaml_content['values']
+data=yaml_content['configmap']
 for key, value in data.items():
     print(f'{key}: {value}')
