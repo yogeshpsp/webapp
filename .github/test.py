bbeef897cb1ff0c8be84460981c2fc313d94e9cc
yogeshpsp <67696309+yogeshpsp@users.noreply.github.com>
2023-09-21 14:12:48 +0530

    Update test.py

--- .github/test.py
+++ .github/test.py
@@ -2,5 +2,5 @@ import yaml
 with open ('test.yaml','r') as file:
   yaml_content=yaml.safe_load(file)
 data=yaml_content['values']
-for key, value in data.items()
+for key, value in data.items():
     print(f'{key}: {value}')
