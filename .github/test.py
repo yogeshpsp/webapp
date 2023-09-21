ec2ef5437ae7e7201e7897f59c6cf9e6566d8860
yogeshpsp <67696309+yogeshpsp@users.noreply.github.com>
2023-09-21 14:28:14 +0530

    Update test.py

--- .github/test.py
+++ .github/test.py
@@ -1,6 +1,6 @@
 import yaml
 with open ('test.yaml','r') as stream:
-  yaml_content=yaml.safe_load(file)
+  yaml_content=yaml.safe_load(stream)
   if 'value' in value:
     for key, value in value['value'].items():
       print(f"{key}: {value}")
